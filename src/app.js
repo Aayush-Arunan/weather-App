f5e0e207460bdece515f17f4c5a8ca4d012363e5
unknown <arunanayush@gmail.com>
2025-10-08 09:59:22 +0530

    update files

--- src/app.js
+++ src/app.js
@@ -26,12 +26,12 @@ async function getFiveDayForecast(city) {
 
     // Map weather conditions to image paths
     const weatherImages = {
-      rain: "images/rain.png",
-      snow: "images/snow.png",
-      clear: "images/hot.png", // "Clear" is the term used by OpenWeatherMap so to understand i have used it
-      clouds: "images/cloudy.png", 
-      drizzle: "images/drizzle.png", 
-      thunderstorm: "images/storm.png" 
+      rain: "src/images/rain.png",
+      snow: "src/images/snow.png",
+      clear: "src/images/hot.png", // "Clear" is the term used by OpenWeatherMap so to understand i have used it
+      clouds: "src/images/cloudy.png", 
+      drizzle: "src/images/drizzle.png", 
+      thunderstorm: "src/images/storm.png" 
     };
 
     const defaultImage = "images/default.jpg";
